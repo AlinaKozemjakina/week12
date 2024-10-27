20b63baea7fa4039156673a25a94ec7005be0357
Anastassia Medvedeva <anastassia.medvedeva@tktk.ee>
2024-10-27 20:58:05 +0200

    week10 code

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const itemsPool = new Pool({
     user: 'postgres',
-    password: 'admin1234',
+    password: 'Nastjacveto4ek26',
     host: 'localhost',
     port: 5432,
     database: 'myrecipes'
