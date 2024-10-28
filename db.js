0f579ebb454163a204af22a619004bb068063c3e
Anastassia Medvedeva <anastassia.medvedeva@tktk.ee>
2024-10-28 17:04:02 +0200

    week 11 files

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
-const itemsPool = new Pool({
-    user: 'postgres',
-    password: 'Nastjacveto4ek26',
-    host: 'localhost',
-    port: 5432,
-    database: 'myrecipes'
-});
+
+ const itemsPool = new Pool({
+     user: 'postgres',
+     password: 'Nastjacveto4ek26',
+     host: 'localhost',
+     port: 5432,
+     database: 'myrecipes'
+ });
 
 module.exports = itemsPool;
